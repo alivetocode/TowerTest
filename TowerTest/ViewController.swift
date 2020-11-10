9d81c6f7a720e8f88e6708f52d008e99abc9199e
Scott Harman <scott.harman@willowtreeapps.com>
2020-11-10 05:27:42 -0500

    Initial Commit

---
+++ TowerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TowerTest
+//
+//  Created by Scott Harman on 11/10/20.
+//  Copyright © 2020 Scott Harman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
